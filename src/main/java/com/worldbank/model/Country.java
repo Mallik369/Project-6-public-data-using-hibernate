d03ee3938c97d64913ad58693adfa7662af1c1a1
Mallikarjuna <drop2ms@gmail.com>
2016-11-13 16:06:16 +0530

    Defined Country Model Class associated with DataBase

---
+++ src/main/java/com/worldbank/model/Country.java
@@ -0,0 +1,65 @@
+package com.worldbank.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Country {
+    @Id
+    private String code;                    // country code
+    private String name;                    // country name
+    private Double internetUsers;           // country Internet Users
+    private Double adultLiteracyRate;       // country Adult Literacy Rate
+
+    public Country() {}
+
+    public Country(CountryBuilder builder) {
+        this.code = builder.code;
+        this.name = builder.name;
+        this.internetUsers = builder.internetUsers;
+        this.adultLiteracyRate = builder.adultLiteracyRate;
+    }
+
+    public String getCode() {return code;}
+
+    public void setCode(String code) {this.code = code;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public Double getInternetUsers() {return internetUsers;}
+
+    public void setInternetUsers(Double internetUsers) {this.internetUsers = internetUsers;}
+
+    public Double getAdultLiteracyRate() {return adultLiteracyRate;}
+
+    public void setAdultLiteracyRate(Double adultLiteracyRate) {this.adultLiteracyRate = adultLiteracyRate;}
+
+    public static class CountryBuilder {
+
+        private String code;
+        private String name;
+        private Double internetUsers;
+        private Double adultLiteracyRate;
+
+        public CountryBuilder(String code,String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public CountryBuilder withInternetUsers(Double internetUsers) {
+            this.internetUsers = internetUsers;
+            return this;
+        }
+
+        public CountryBuilder withAdultLiteracyRate(Double adultLiteracyRate){
+            this.adultLiteracyRate = adultLiteracyRate;
+            return this;
+        }
+
+        public Country build( ){
+            return new Country(this);
+        }
+    }
+}
